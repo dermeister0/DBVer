9f64dbf6cd14efd1a10bfa1588286ad8fddf2caa
Anton Zimin <anton@saritasa.com>
2015-12-22 11:40:44 +0700

    Implement name replacement. Don't export objects with the same name again.

---
+++ src/DBVer/Mapping/ProcessedMap.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+
+namespace DBVer.Mapping
+{
+    internal class ProcessedMap
+    {
+        private readonly HashSet<string> map = new HashSet<string>();
+
+        public void Add(string schema, string objectName, ObjectType objectType)
+        {
+            map.Add(GetFullName(schema, objectName, objectType));
+        }
+
+        public bool Contains(string schema, string objectName, ObjectType objectType)
+        {
+            return map.Contains(GetFullName(schema, objectName, objectType));
+        }
+
+        private string GetFullName(string schema, string objectName, ObjectType objectType)
+        {
+            return $"{objectName}_{schema}.{objectType}";
+        }
+    }
+}
