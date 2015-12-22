a786a8376a56aa630ae9cd386d09088af666e3c6
Anton Zimin <anton@saritasa.com>
2015-12-22 17:25:03 +0700

    Enhance parallel code.

---
+++ src/DBVer/ServerInfo.cs
@@ -0,0 +1,44 @@
+﻿using System.Text;
+using Microsoft.SqlServer.Management.Common;
+using Microsoft.SqlServer.Management.Smo;
+
+namespace DBVer
+{
+    class ServerInfo
+    {
+        private readonly Server server;
+        private Scripter scripter;
+
+        public ServerInfo(string serverHost, string userName, string password)
+        {
+            var connection = new ServerConnection(serverHost, userName, password) { NonPooledConnection = true, AutoDisconnectMode = AutoDisconnectMode.NoAutoDisconnect };
+            server = new Server(connection);
+            server.ConnectionContext.Connect();
+            CreateScripter();
+        }
+
+        public Server Server => server;
+        public Scripter Scripter => scripter;
+
+        void CreateScripter()
+        {
+            scripter = new Scripter(server);
+            scripter.Options = new ScriptingOptions
+                {
+                    IncludeHeaders = false,
+                    Indexes = true,
+                    DriAllKeys = true,
+                    NoCollation = false,
+                    SchemaQualify = true,
+                    SchemaQualifyForeignKeysReferences = true,
+                    Permissions = false,
+                    Encoding = Encoding.UTF8
+                };
+        }
+
+        public void Disconnect()
+        {
+            server.ConnectionContext.Disconnect();
+        }
+    }
+}
